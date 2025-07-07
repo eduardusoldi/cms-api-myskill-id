22209390856293bab85c84ff91553c55b662958d
eduancuk <eduardusoldi@outlook.com>
2025-07-08 02:00:28 +0700

    feat: adding env file for jwt and mongo uri

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/myskill-id'
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/myskill-id');
+    await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err);
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,36 +8,20 @@ const SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 export const login = async (req: Request, res: Response): Promise<void> => {
   const { username, password } = req.body;
 
-  try {
-    const user = await User.findOne({ username });
-    if (!user) {
-      console.log('User not found');
-      res.status(401).json({ message: 'Invalid credentials' });
-      return;
-    }
-
-    console.log('Login attempt:', { username });
-    console.log('DB Password Hash:', user.password);
-
-    const isValidPassword = await bcrypt.compare(password, user.password);
-    console.log('Password match?', isValidPassword);
-
-    if (!isValidPassword) {
-      res.status(401).json({ message: 'Invalid credentials' });
-      return;
-    }
-
-    const token = jwt.sign(
-      { id: user._id, username: user.username },
-      SECRET,
-      { expiresIn: '1h' }
-    );
-
-    res.json({ token });
-  } catch (error) {
-    console.error('Login error:', error);
-    res.status(500).json({ message: 'Internal server error' });
+  const user = await User.findOne({ username });
+  if (!user) {
+    res.status(401).json({ message: 'Invalid credentials' });
+    return;
   }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    res.status(401).json({ message: 'Invalid credentials' });
+    return;
+  }
+
+  const token = jwt.sign({ id: user._id, username: user.username }, SECRET, { expiresIn: '1h' });
+  res.json({ token });
 };
 
 export const logout = (_req: Request, res: Response): void => {
